782d7e04aa5efdff2015f64ef82f6bb7f5efa136
OriTeicher <oriteicherudemy@gmail.com>
2024-07-19 11:01:27 +0300

    fix ui

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Navbar/Navbar';
 import { NoteIndexComponent } from './NoteIndex/NoteIndex';
 import { SidebarComponent } from './Sidebar/Sidebar';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +15,11 @@ import { SidebarComponent } from './Sidebar/Sidebar';
     NavbarComponent,
     SidebarComponent,
     SidebarComponent,
+    NgIconComponent,
   ],
   templateUrl: './App.html',
   styleUrl: './App.scss',
+  viewProviders: [provideIcons({ heroUsers })],
 })
 export class AppComponent {
   title = 'Googel Keep';
