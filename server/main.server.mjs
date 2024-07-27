2161fe37f05cedb1c4832cc73470d6b5e5668768
OriTeicher <oriteicherudemy@gmail.com>
2024-07-27 21:05:34 +0300

    Updates

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CAQBFODN.mjs";import"./chunk-NXVMNOAK.mjs";import"./chunk-K3JU7OIB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-SWOXL6P6.mjs";import"./chunk-NXVMNOAK.mjs";import"./chunk-K3JU7OIB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
